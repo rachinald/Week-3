f2b6167ee1998c635140c1e84b98cd6ff64229d6
Rachael Jane <rachinald@gmail.com>
2021-12-03 11:52:32 +0000

    fifth comment

--- dice/app.js
+++ dice/app.js
@@ -1,19 +1,55 @@
-// this will generate a random number between 1 - 6
-const RandomNum = Math.floor(Math.random() *6) +1
+// // this will generate a random number between 1 - 6
+const randomNum = Math.floor(Math.random() *6) +1
 
 // This creates the image by adding .png to dice plus random number, this is genius!
-const firstDiceImage = "assets/dice" + RandomNum + ".png";
+const randomDiceImage = "assets/dice" + randomNum + ".png";
 
-const score = document.getElementById("score");
+const score = document.getElementById("score") 
 const message = document.getElementById("message");
 
+
+
+
+// function for gamePlaying (init = initialise game)
+
+// play () = > {
+
+// reseting score vars
+
+// scores = [0, 0];
+// roundScore = 0,
+// gameplaying = true;
+
+// randomDiceImage.style.display = "none";
+
+// }
+
+
+
+
+// function for roll button
 
 button.addEventListener("click",()=>{
 
   
-  document.querySelectorAll('img')[0].setAttribute("src", firstDiceImage);
-  
-  score.innerHTML = RandomNum;
+
+ // displays dice image
+  document.querySelectorAll('img')[0].setAttribute("src", randomDiceImage);
+
+  // generates a random number and puts it into the score
+  score.innerHTML = randomNum;
+
+  // += adds the value of the right operand to a variable and assigns the result to the variable.
+  // score += randomNum;
+
+  //   message.innerHTML = "Cool! Roll again!";
+
+  // }
+
+  // else {
+
+  //   message.innerHTML = "You lose!";
+  // }
 
 
 });
